8c35afd31e8f13f5fc4b6ff699786f394dc626e4
bard <schy2503@naver.com>
2022-07-20 17:51:16 +0900

    add: 프로젝트 생성

---
+++ Atrtistep/Atrtistep/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Atrtistep
+//
+//  Created by 김동용 on 2022/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
