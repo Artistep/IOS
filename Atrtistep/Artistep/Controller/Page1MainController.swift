ea521d8da768f0781e097d36a782c7d332263249
sozohoy <sozohoy@gmail.com>
2022-08-18 20:27:26 +0900

    feat: Main - TableView

--- Atrtistep/Artistep/Controller/Page1MainController.swift
+++ Atrtistep/Artistep/Controller/Page1MainController.swift
@@ -9,17 +9,43 @@ import UIKit
 
 class Page1MainController: UIViewController {
 
+    //MARK: IBOutlet
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: Properties
+    
+    
+    //MARK: func
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: Button Action
+    
+    
+    //MARK: etc function
+    
+    
 
 
 }
 
-//MARK: TableView Extension
+//MARK: Extension TableView
 
 extension Page1MainController: UITableViewDelegate, UITableViewDataSource {
+    
+    //MARK: TableView Method
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
     
     
 }
