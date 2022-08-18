b2c4eee2b0736f58b6cbaa3dab2b19e4f3234c05
sozohoy <sozohoy@gmail.com>
2022-08-18 20:27:53 +0900

    add: Page1MainTableViewCell.swift & .xib 추가

---
+++ Atrtistep/Artistep/View/Page1MainTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  Page1MainTableViewCell.swift
+//  Artistep
+//
+//  Created by 한지석 on 2022/08/18.
+//
+
+import UIKit
+
+class Page1MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
