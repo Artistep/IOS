e314ed4bc241daff1ae8983db01083dd92b24f26
sozohoy <sozohoy@gmail.com>
2022-08-19 20:08:05 +0900

    feat: TableViewCell update

--- Atrtistep/Artistep/Controller/Page1MainController.swift
+++ Atrtistep/Artistep/Controller/Page1MainController.swift
@@ -9,23 +9,24 @@ import UIKit
 
 class Page1MainController: UIViewController {
 
-    //MARK: IBOutlet
+    //MARK: IBOutlet properties
     
     @IBOutlet weak var tableView: UITableView!
     
     //MARK: Properties
     
     
-    //MARK: func
+    //MARK: Functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerXib()
     }
     
-    //MARK: Button Action
+    //MARK: Button actions
     
     
-    //MARK: etc function
+    //MARK: etc functions
     
     private func registerXib(){
         let nib = UINib(nibName: "Page1MainTableViewCell", bundle: nil)
@@ -38,7 +39,7 @@ class Page1MainController: UIViewController {
 
 extension Page1MainController: UITableViewDelegate, UITableViewDataSource {
     
-    //MARK: TableView Method
+    //MARK: TableView method
     
     // 테이블 뷰에 보여줄 갯수. 호출 이후 추가 될 때 마다 배열에 append --> reload
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -47,8 +48,9 @@ extension Page1MainController: UITableViewDelegate, UITableViewDataSource {
     
     // 테이블 뷰에 올라간 셀의 형태
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "mainXib", for: indexPath)
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mainXib", for: indexPath) as! Page1MainTableViewCell
+        cell.userID.text = "@sozohoy"
+        cell.userNickName.text = "석지한"
         return cell
     }
     
--- Atrtistep/Artistep/View/Page1MainTableViewCell.swift
+++ Atrtistep/Artistep/View/Page1MainTableViewCell.swift
@@ -9,8 +9,19 @@ import UIKit
 
 class Page1MainTableViewCell: UITableViewCell {
 
+    //MARK: IBOutlet properties
+    
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userNickName: UILabel!
+    @IBOutlet weak var userID: UILabel!
+    
+    //MARK: Properties
+    
+    //MARK: Functions
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        designAdjustment()
         // Initialization code
     }
 
@@ -18,6 +29,11 @@ class Page1MainTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    private func designAdjustment(){
+        userImage.layer.borderWidth = 1.0
+        userImage.layer.cornerRadius = 25
     }
     
 }
