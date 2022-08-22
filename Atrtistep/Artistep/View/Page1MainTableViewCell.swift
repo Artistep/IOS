1e4cd440528b2176dce79e472c67923b23e1c20f
sozohoy <sozohoy@gmail.com>
2022-08-22 17:55:07 +0900

    style: TableViewCell Button

--- Atrtistep/Artistep/View/Page1MainTableViewCell.swift
+++ Atrtistep/Artistep/View/Page1MainTableViewCell.swift
@@ -32,7 +32,12 @@ class Page1MainTableViewCell: UITableViewCell {
         contentView.layer.cornerRadius = 10
         contentView.layer.borderColor = UIColor.white.cgColor
         
-        contentView.frame
+//        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 15,
+//                                                                     left: 0,
+//                                                                     bottom: 0,
+//                                                                     right: 0))
+//
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -42,11 +47,13 @@ class Page1MainTableViewCell: UITableViewCell {
     }
     
     private func designAdjustment(){
+        // make circle
         userImage.layer.cornerRadius = userImage.frame.width / 2
         userImage.clipsToBounds = true
         userImage.layer.borderWidth = 0.7
         userImage.layer.borderColor = UIColor.gray.cgColor
         
+        // video
         testVideo.layer.cornerRadius = 10
     }
     
