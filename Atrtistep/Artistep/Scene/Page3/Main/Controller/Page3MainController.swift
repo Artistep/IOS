5db3d27c383a2e059181114fa0d1cc96b30769c3
shin seung jae <shinseungjae1234@gmail.com>
2022-09-06 13:00:39 +0900

    add: Main/MVC파일 추가

---
+++ Atrtistep/Artistep/Scene/Page3/Main/Controller/Page3MainController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Artistep
+//
+//  Created by 신승재 on 2022/09/06.
+//
+
+import UIKit
+
+class Page3MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
