d5c6b9471beddc275520415febeee23c51150acb
sozohoy <sozohoy@gmail.com>
2022-09-07 19:19:44 +0900

    feat: 영상 크기 추적기능 구현

--- Atrtistep/Artistep/Scene/Page1/Main/Controller/MainController.swift
+++ Atrtistep/Artistep/Scene/Page1/Main/Controller/MainController.swift
@@ -58,20 +58,39 @@ extension MainController: UITableViewDelegate, UITableViewDataSource {
         cell.userID.text = "@sozohoy"
         cell.userNickName.text = "석지한"
         
-        let videoAsset = AVURLAsset(url: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4")
+        let videoAsset = AVURLAsset(url: URL(string: "https://thumbs.gfycat.com/FoolhardyMiserlyAsiantrumpetfish-mobile.mp4")!)
         let videoAssetTrack = videoAsset.tracks(withMediaType: .video).first
         
-        let player = AVPlayer(url: URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4")!)
-        //
-        // https://thumbs.gfycat.com/FoolhardyMiserlyAsiantrumpetfish-mobile.mp4
+        if let naturalSize = videoAssetTrack?.naturalSize{
+            print(naturalSize)
+            if naturalSize.width > naturalSize.height{
+             
+                cell.userVideo.frame = CGRect(x: self.view.frame.minX,
+                                              y: self.view.frame.minY,
+                                              width: self.view.frame.width,
+                                              height: 270)
+
+            } else {
+                
+                cell.userVideo.frame = CGRect(x: self.view.frame.minX,
+                                              y: self.view.frame.minY,
+                                              width: self.view.frame.width,
+                                              height: 800)
+                
+            }
+        }
+        
+        let player = AVPlayer(url: URL(string: "https://thumbs.gfycat.com/FoolhardyMiserlyAsiantrumpetfish-mobile.mp4")!)
+        // https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4 -- 4:3
+        // https://thumbs.gfycat.com/FoolhardyMiserlyAsiantrumpetfish-mobile.mp4 -- 9:16 -> 3:4
         let playerLayer = AVPlayerLayer(player: player)
-        print(player.status.rawValue)
+        
         
         playerLayer.videoGravity = .resizeAspect
         playerLayer.needsDisplayOnBoundsChange = true
         playerLayer.frame.size = CGSize(width: UIScreen.main.bounds.width - 36,
                                         height: cell.userVideo.bounds.height)
-        print(player.accessibilityFrame.height)
+        
         cell.userVideo.layer.addSublayer(playerLayer)
         player.play()
         return cell
