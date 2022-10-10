a7d55e6148561127c8e92fa765cf0e8aae9682a7
shin seung jae <shinseungjae1234@gmail.com>
2022-10-10 14:58:20 +0900

    style: View 폴더 정리

---
+++ Atrtistep/Artistep/Scene/Page3/Main/View/Page3MainTableViewSectionHeader/Page3MainTableViewSectionHeader.swift
@@ -0,0 +1,36 @@
+//
+//  Page3MainTableViewSectionHeader.swift
+//  Artistep
+//
+//  Created by 신승재 on 2022/09/14.
+//
+
+import UIKit
+
+class Page3MainTableViewSectionHeader: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var sectionTitle: UILabel!
+    @IBOutlet weak var viewMoreButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        // viewMoreButton.backgroundColor = UIColor(red: 59/255, green: 59/255, blue: 59/255, alpha: 1)
+        
+        
+        
+
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0,
+                                                                     left: 0,
+                                                                     bottom: 0,
+                                                                     right: 0))
+    // UUID !
+    }
+
+}
